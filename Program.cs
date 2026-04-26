43348f06e35f0768d8b40dff31b0e272842aa77f
Keabetsoe <kbzamatsaneng28@gmail.com>
2026-04-26 23:19:40 +0200

    Revert "Question 1 comments"
    This reverts commit 9b2ced50d05a731a28c4ec76dc833c58fd152afb.

--- Program.cs
+++ Program.cs
@@ -4,24 +4,21 @@ class Program
 
     static void Main()
     {
-        ////The user inputs the student's name as well as the marks for all 3 subjects
-        Console.Write("Enter student name: "); 
+        Console.Write("Enter student name: "); //The user inputs the student name
         string name = Console.ReadLine();
         Console.Write("Enter mark for subject 1: ");
-        double subject1 = double.Parse(Console.ReadLine()); 
+        double subject1 = double.Parse(Console.ReadLine()); //user inputs the marks for the first subject
         Console.Write("Enter mark for subject 2: ");
-        double subject2 = double.Parse(Console.ReadLine()); 
+        double subject2 = double.Parse(Console.ReadLine()); //user inputs the marks for the second subject
         Console.Write("Enter mark for subject 3: ");
-        double subject3 = double.Parse(Console.ReadLine());
+        double subject3 = double.Parse(Console.ReadLine()); //user inputs the marks for the third subject
 
-        //skips a line within the console
-        Console.WriteLine(); 
 
-        //Displays the student name, calculates the sum of all marks as well as the average
+        Console.WriteLine(); //skips a line within the console
         Console.WriteLine("========== STUDENT RESULTS ========");
-        Console.WriteLine("Student name: " + name); 
-        Console.WriteLine("Total Marks: " + (subject1 + subject2 + subject3));
-        Console.WriteLine("Average Marks: " + ((subject1 + subject2 + subject3) / 3)); 
+        Console.WriteLine("Student name: " + name); //displays the students name
+        Console.WriteLine("Total Marks: " + (subject1 + subject2 + subject3)); // sums all the marks to get the total
+        Console.WriteLine("Average Marks: " + ((subject1 + subject2 + subject3) / 3)); //calculates the average for the marks
 
         //determines whether or not the aforementioned student has passed
         if (((subject1 + subject2 + subject3) / 3) > 50)
