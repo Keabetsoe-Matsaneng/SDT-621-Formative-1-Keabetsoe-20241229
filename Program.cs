9b2ced50d05a731a28c4ec76dc833c58fd152afb
Keabetsoe <kbzamatsaneng28@gmail.com>
2026-04-26 23:16:58 +0200

    Question 1 comments

--- Program.cs
+++ Program.cs
@@ -4,21 +4,24 @@ class Program
 
     static void Main()
     {
-        Console.Write("Enter student name: "); //The user inputs the student name
+        ////The user inputs the student's name as well as the marks for all 3 subjects
+        Console.Write("Enter student name: "); 
         string name = Console.ReadLine();
         Console.Write("Enter mark for subject 1: ");
-        double subject1 = double.Parse(Console.ReadLine()); //user inputs the marks for the first subject
+        double subject1 = double.Parse(Console.ReadLine()); 
         Console.Write("Enter mark for subject 2: ");
-        double subject2 = double.Parse(Console.ReadLine()); //user inputs the marks for the second subject
+        double subject2 = double.Parse(Console.ReadLine()); 
         Console.Write("Enter mark for subject 3: ");
-        double subject3 = double.Parse(Console.ReadLine()); //user inputs the marks for the third subject
+        double subject3 = double.Parse(Console.ReadLine());
 
+        //skips a line within the console
+        Console.WriteLine(); 
 
-        Console.WriteLine(); //skips a line within the console
+        //Displays the student name, calculates the sum of all marks as well as the average
         Console.WriteLine("========== STUDENT RESULTS ========");
-        Console.WriteLine("Student name: " + name); //displays the students name
-        Console.WriteLine("Total Marks: " + (subject1 + subject2 + subject3)); // sums all the marks to get the total
-        Console.WriteLine("Average Marks: " + ((subject1 + subject2 + subject3) / 3)); //calculates the average for the marks
+        Console.WriteLine("Student name: " + name); 
+        Console.WriteLine("Total Marks: " + (subject1 + subject2 + subject3));
+        Console.WriteLine("Average Marks: " + ((subject1 + subject2 + subject3) / 3)); 
 
         //determines whether or not the aforementioned student has passed
         if (((subject1 + subject2 + subject3) / 3) > 50)
